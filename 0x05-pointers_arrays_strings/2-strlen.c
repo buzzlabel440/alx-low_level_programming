d6428fd589091f3a89b148700ee74d76e17492ee
buzzlabel440 <buzzlabel79@gmail.com>
2022-12-19 06:11:30 -0800

    commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen -> functin to get the length of a string
+ * @s: string pointer to passed to this function
+ * Return: returns length of a string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
